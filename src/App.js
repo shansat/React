15c4949586ee8466bd5fcb35a6a30fddb90e4729
shansat <shansat47@gmail.com>
2022-06-10 12:39:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [theme, setTheme] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? "dark-theme": ""}>
+
+      <div className="content-bg-color main-content">
+        <h1>Dark Mode is </h1>
+        <input type="checkbox" onChange={() => setTheme(!theme)}/>
+      </div>
     </div>
   );
 }
